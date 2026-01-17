55472f6d2b2ae871f2a1ab1d0633e1d2822a0cbf
Bing Hang <bings@Bings-MacBook-Pro.local>
2026-01-17 18:12:56 +0800

    Stop tracking appsettings.Development.json

--- obj/Debug/net10.0/who-took-it-backend.AssemblyInfo.cs
+++ obj/Debug/net10.0/who-took-it-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("who-took-it-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52ab05b4b9dc7bc2c82f992c8c2a23857532d88f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf9bf104ddadc5a116d1ac920e2f2ed3634c3e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("who-took-it-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("who-took-it-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
